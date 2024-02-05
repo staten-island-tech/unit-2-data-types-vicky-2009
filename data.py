68d04e551bb1a436ef592bf8e2d7a9db078c21fb
vicky-2009 <vickyo4@nycstudents.net>
2024-02-05 12:02:39 -0500

    data

---
+++ data.py
@@ -0,0 +1,25 @@
+#Data Types
+#Numbers 1,2,3
+
+def add(x,y):
+    print(x + y)
+add(1,2)
+
+#strings "a,b,c"
+name = "Vicky"
+def greeting(person):
+    print(person)
+greeting(name)
+
+#1 and "1" are not the same
+add("1","2")
+#undefined/null
+
+#booLeans
+tenure = False
+def is_tenured(status):
+    if(status == True):
+        print("They have tenure")
+    else:
+        print("they are not tenured")
+
---
+++ lists.py
@@ -0,0 +1,22 @@
+
+ 
+animals = ["Zebra","Camel","Ape"]
+#start count at 0, reference each element with []
+print(animals[0])
+for animal in animals:
+    print(animal)
+ 
+
+for animal in animals:
+    if (animal == "Camel"):
+        print("We're in the desert")
+
+
+#Strings operate like Lists
+z = "Hello Freshman, you all smell"
+print(x[0])
+y = x.upper()
+print(y)
+
+words_list = x.split(",")
+print(words_list)
