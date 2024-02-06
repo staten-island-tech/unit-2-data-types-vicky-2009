ce8222d7620bf7618167303c2c6d0b7ff1af8c70
vicky-2009 <vickyo4@nycstudents.net>
2024-02-06 12:28:06 -0500

    2/6/24

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-#Data Types
+""" #Data Types
 #Numbers 1,2,3
 
 def add(x,y):
@@ -22,4 +22,24 @@ def is_tenured(status):
         print("They have tenure")
     else:
         print("they are not tenured")
+ """
 
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+print(values[0])
+print(values[6])
+ """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+value = [5]
--- lists.py
+++ lists.py
@@ -1,4 +1,4 @@
-
+""" 
  
 animals = ["Zebra","Camel","Ape"]
 #start count at 0, reference each element with []
@@ -13,10 +13,32 @@ for animal in animals:
 
 
 #Strings operate like Lists
-z = "Hello Freshman, you all smell"
+x = "Hello Freshman, you all smell"
 print(x[0])
 y = x.upper()
 print(y)
 
 words_list = x.split(",")
-print(words_list)
+print(words_list) """
+
+""" day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+
+""" temp = 57
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
+ """
+
+number = input("5")
+if (number):
+    print("odd")
+else:
+    print("even")
