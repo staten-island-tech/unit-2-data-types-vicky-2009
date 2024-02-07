91ca10110922037a1168ad761aaf82c753cd2c80
vicky-2009 <vickyo4@nycstudents.net>
2024-02-07 12:28:22 -0500

    calculator

--- lists.py
+++ lists.py
@@ -37,8 +37,40 @@ else:
     print('cold')
  """
 
-number = input("5")
-if (number):
+""" #challenge 1
+num = int(input("number:"))
+if (num%2):
     print("odd")
 else:
-    print("even")
+    print("even") """
+
+
+#calculator
+
+bill = input ("Bill:57")
+tip = input("Tip:0.1")
+ttl = float(bill)+float(tip)
+print (ttl) 
+
+
+
+
+
+
+
+""" #challenge 2
+bill = float(input ("Bill?"))
+service = input("How is the service?")
+tp = bill*0
+tp1 = bill*0.15
+tp2 = bill*0.20
+tp3 = bill*0.25
+
+if service == "Bad":
+    print(bill+tp)
+elif service == "Okay":
+    print(bill+tp1)
+elif service == "Good":
+    print(bill+tp2)
+elif service == "Great":
+    print(bill+tp3) """
